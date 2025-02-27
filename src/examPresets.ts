853a8e3ee4aecba778b442ea04d7324b110ffddf
dtinth on MBP M1 <dtinth@spacet.me>
2025-02-27 14:59:28 +0700

    add info about each exam preset

--- src/examPresets.ts
+++ src/examPresets.ts
@@ -4,32 +4,44 @@ import { readFileSync } from "node:fs";
 const examPresetDefinitions: Record<string, ExamPresetDefinition> = {
   onet: {
     filePaths: ["thai_exam/data/onet/onet_test.jsonl"],
+    title: "O-NET: Ordinary National Educational Test (ชั้นมัธยมศึกษาปีที่ 6)",
+    description: "การทดสอบทางการศึกษาระดับชาติขั้นพื้นฐาน",
   },
   a_level: {
     filePaths: ["thai_exam/data/a_level/a_level_test.jsonl"],
+    title: "A-Level: Applied Knowledge Level",
+    description: "การทดสอบความรู้เชิงวิชาการระดับประยุกต์",
   },
   ic: {
     filePaths: ["thai_exam/data/ic/ic_test.jsonl"],
+    title: "IC: Investment Consultant",
+    description: "การทดสอบใบอนุญาตผู้แนะนำการลงทุน",
   },
   tgat: {
     filePaths: ["thai_exam/data/tgat/tgat_test.jsonl"],
+    title: "TGAT: Thai General Aptitude Test",
+    description: "การทดสอบความถนัดทั่วไป",
   },
   tpat1: {
     filePaths: ["thai_exam/data/tpat1/tpat1_test.jsonl"],
+    title: "TPAT-1: The Thai Professional Aptitude Test 1",
+    description: "การทดสอบความถนัดเฉพาะด้านวิชาชีพ 1: (ความถนัดแพทย์)",
   },
 };
 
 interface ExamPresetDefinition {
   filePaths: string[];
+  title: string;
+  description: string;
 }
 
 class ExamPresets {
   private presets: Map<string, ExamPreset> = new Map();
-  
+
   get availableExamPresetIds() {
     return Object.keys(examPresetDefinitions);
   }
-  
+
   get(id: string) {
     return getOrCreate(this.presets, id, () => {
       const definition = examPresetDefinitions[id];
